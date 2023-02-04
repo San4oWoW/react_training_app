cdb0ef6a220579b8f4845b0692d301be7a0b8097
Alex <alexalikin28@gmail.com>
2023-02-04 15:32:11 +0500

    01:04:27

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import Counter from "./components/Counter";
 import './styles/App.css'
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import PostItem from "./components/PostItem";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
 
@@ -12,17 +15,18 @@ function App() {
         {id: 3, title: 'Javascript 3', body: "Description"}
     ])
 
-    const [posts2, setPosts2] = useState([
-        {id: 1, title: 'Python', body: "Description"},
-        {id: 2, title: 'Python 2', body: "Description"},
-        {id: 3, title: 'Python 3', body: "Description"}
-    ])
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
 
   return (
    <div className="App">
-        <PostList posts={posts} title="Список постов про JS"/>
-        <PostList posts={posts2} title="Список постов про Python"/>
-        <Counter/>
+        <PostForm create={createPost}></PostForm>
+       <PostList remove={removePost} posts={posts} title={"Посты про ЯП"}/>
    </div>
   );
 }
